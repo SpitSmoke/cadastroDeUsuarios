b050592ae9747117fd69d8f6a97f811680e7e178
SpitSmoke <jmfelicio.sp@gmail.com>
2024-09-27 20:03:55 -0300

    BaseURL changed

--- cadastroDeUsuarios/src/services/api.js
+++ cadastroDeUsuarios/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const api =axios.create({
-    baseURL: 'http://localhost:3000',
-})
+const api = axios.create({
+    baseURL: 'https://cadastro-de-usuarios-production.up.railway.app', 
+});
 
-export default api
+export default api;
